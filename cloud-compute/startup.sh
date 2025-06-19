fd2fdcc58e2fdd0f3f9bf0e946f556e25bb5c565
ltp1405 <lamtungpro1405@gmail.com>
2025-06-19 12:01:44 +0700

    add lock everywhere

--- cloud-compute/startup.sh
+++ cloud-compute/startup.sh
@@ -31,12 +31,15 @@ then
     echo "Installing Docker..."
     apt-get update
     echo "Installing prerequisites..."
+    wait_for_apt_locks
     apt-get install -y apt-transport-https ca-certificates curl gnupg
     curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker.gpg
     echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker.gpg] https://download.docker.com/linux/debian bookworm stable" | sudo tee \
         /etc/apt/sources.list.d/docker.list > /dev/null
+    wait_for_apt_locks
     apt-get update
     echo "Installing Docker packages..."
+    wait_for_apt_locks
     apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
     # For startup scripts, the user is root, so this might not be strictly necessary for the script itself,
